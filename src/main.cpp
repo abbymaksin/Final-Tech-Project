06fd4109c511ae318e81de6de0bbaca536e6f05a
David Aronson <aronsond@ramaz.org>
2025-04-29 14:21:30 -0400

    got it to measeure heart rate etc

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,46 @@
 #include <Arduino.h>
 #include <Seeed_Arduino_mmWave.h>
 //
-// put function declarations here:
-int myFunction(int, int);
+
+// Set up serial communication depending on the board type
+#ifdef ESP32
+#  include <HardwareSerial.h>
+HardwareSerial mmWaveSerial(0);
+#else
+#  define mmWaveSerial Serial1
+#endif
+
+SEEED_MR60BHA2 mmWave;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Serial.begin(115200);
+mmWave.begin(&mmWaveSerial);
+Serial.printf("setup");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+if (mmWave.update(100)) {
+ float total_phase, breath_phase, heart_phase;
+ if (mmWave.getHeartBreathPhases(total_phase, breath_phase, heart_phase)) {
+   Serial.printf("total_phase: %.2f\t", total_phase);
+   Serial.printf("breath_phase: %.2f\t", breath_phase);
+   Serial.printf("heart_phase: %.2f\n", heart_phase);
+ }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+ float breath_rate;
+ if (mmWave.getBreathRate(breath_rate)) {
+   Serial.printf("breath_rate: %.2f\n", breath_rate);
+ }
+
+ float heart_rate;
+ if (mmWave.getHeartRate(heart_rate)) {
+   Serial.printf("heart_rate: %.2f\n", heart_rate);
+ }
+
+ float distance;
+ if (mmWave.getDistance(distance)) {
+   Serial.printf("distance: %.2f\n", distance);
+ }
+}
+delay(1000);
 }
